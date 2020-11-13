201f0c51f9d192d977f22ab2dee181fb0cdd3735
Arunscape <arun@woosaree.xyz>
2020-11-12 18:17:21 -0700

    run and jump animations

--- Player.gd
+++ Player.gd
@@ -2,8 +2,9 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 200
 const JUMP_HEIGHT = -500
+const RUN_ACCELERATION = 50
+const MAX_SPEED = 200
 
 var motion = Vector2()
 # Declare member variables here. Examples:
@@ -23,18 +24,33 @@ func _ready():
 func _physics_process(delta):
 	
 	motion.y += GRAVITY
+	var friction = false
 	
 	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
-		motion.x = SPEED
+		motion.x = min(motion.x + RUN_ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
-		motion.x = -SPEED
+		motion.x = max(motion.x - RUN_ACCELERATION, -MAX_SPEED)
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
-		motion.x =0 
+		friction = true
+		$Sprite.play("Idle")
 		
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		print("jump")
-		motion.y += JUMP_HEIGHT
-		
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y += JUMP_HEIGHT
+			
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.2)
+	else:
+		$Sprite.play("Jump")
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.01)
+			
+	
+	
 	motion = move_and_slide(motion, UP)
 	pass
 	
